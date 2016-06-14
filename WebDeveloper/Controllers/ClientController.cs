bda955c46b50307c88ceb423c185b1c585a39087
JeremyDavidValdivia <jeremy.david@jcservicios.com>
2016-06-13 22:27:52 -0500

    13-06-2016: Creación Inicial de CodeFirst

---
+++ WebDeveloper/Controllers/ClientController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using WebDeveloper.DataAccess;
+
+namespace WebDeveloper.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Index()
+        {
+            var client = new ClientData();
+            return View(client.GetList());
+        }
+    }
+}
