077a0777304023f210233ac31aa7d5c147a81919
JeremyDavidValdivia <jeremy.david@jcservicios.com>
2016-06-15 20:26:23 -0500

    Cambios al 2016-06-15 20:26

--- WebDeveloper/Controllers/ClientController.cs
+++ WebDeveloper/Controllers/ClientController.cs
@@ -1,15 +1,35 @@
 ﻿using System.Web.Mvc;
 using WebDeveloper.DataAccess;
+using WebDeveloper.Model;
 
 namespace WebDeveloper.Controllers
 {
     public class ClientController : Controller
     {
+        private ClientData _client = new ClientData();
+
         // GET: Client
         public ActionResult Index()
         {
             var client = new ClientData();
-            return View(client.GetList());
+            return View(_client.GetList());
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Client());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Client client)
+        {
+            /* Validación del lado del servidor - las llamadas a Requered */
+            if (ModelState.IsValid)
+            {
+                _client.Add(client);
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
