6454370fc36b77ec5b3dde8ea9c334727ecf2230
JeremyDavidValdivia <jeremy.david@jcservicios.com>
2016-06-20 20:14:47 -0500

    .

---
+++ WebDeveloper.Model/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDeveloper.Model
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Code : ")]
+        [Required(ErrorMessage = "Code is required")]
+        [StringLength(15)]
+        public string Code{ get; set; }
+
+        [Display(Name = "Description : ")]
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(200)]
+        public string Desc { get; set; }
+
+        private DateTime _dDate = DateTime.MinValue;
+        [Display(Name = "Date : ")]
+        [Required(ErrorMessage = "Date is required")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date, ErrorMessage = "Date only")]
+        public DateTime dDate
+        {
+            get
+            {
+                return (_dDate == DateTime.MinValue) ? DateTime.Now : _dDate;
+            }
+            set { _dDate = value; }
+        }
+        
+        [Display(Name = "Price : ")]
+        [Required(ErrorMessage = "Price is required")]
+        public double Price { get; set; }
+
+        [Display(Name = "Stock : ")]
+        [Required(ErrorMessage = "Stock is required")]
+        public double Stock { get; set; }
+    }
+}
