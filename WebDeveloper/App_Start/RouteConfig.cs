f334c4d9d239f2437a82d9781ffbcae7bb78b64a
JeremyDavidValdivia <jeremy.david@jcservicios.com>
2016-06-22 22:30:14 -0500

    Ejemplos de MapRoute

--- WebDeveloper/App_Start/RouteConfig.cs
+++ WebDeveloper/App_Start/RouteConfig.cs
@@ -14,9 +14,22 @@ namespace WebDeveloper
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Razor",
+                url: "{Razor}/{id}",
+                defaults: new
+                {
+                    controller = "Razor",
+                    action = "Product",
+                    id = UrlParameter.Optional
+                }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home",
+                                action = "Index",
+                                id = UrlParameter.Optional }
             );
         }
     }
